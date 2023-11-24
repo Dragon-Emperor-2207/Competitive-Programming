cba10cfbbb8dd5bb4377c8ff9aa6e49f7852e50d
Dragon-Emperor-2207 <harshsri1602@gmail.com>
2023-11-24 23:19:14 +0530

    Solution to the problem Game with Integers

---
+++ Game_with_Integers.cpp
@@ -0,0 +1,26 @@
+// coding in vengeance until the dragon is set free again
+//vengeance is sweet
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+signed main() {
+    // to make input output fast
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	ll t;
+	cin>>t;
+	while(t--){
+	    int n;cin>>n;
+        if(n%3==2){
+            cout<<"First"<<endl;
+            continue;
+        }
+        if(n%3==1){
+            cout<<"First"<<endl;
+            continue;
+        }
+        cout<<"Second"<<endl;
+
+	}
+	return 0;
+}
