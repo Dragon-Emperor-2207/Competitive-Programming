91ac5c85b1a06bf257606482d2be93c98ef7a9c4
Dragon-Emperor-2207 <harshsri1602@gmail.com>
2023-11-24 23:17:39 +0530

    Solution to the problem Ichihime and Triangle

---
+++ Ichihime_and_Triangle.cpp
@@ -0,0 +1,17 @@
+// coding in vengeance until the dragon is set free again
+//vengeance is sweet
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+signed main() {
+    // to make input output fast
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	ll t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+	}
+	return 0;
+}
